406dc36d67a05f04561faace892cff76759213b8
gxx <562606139@qq.com>
2017-04-12 00:27:57 +0800

    4.12

--- mongoosedome1.js
+++ mongoosedome1.js
@@ -7,15 +7,47 @@ var TestSchema = new mongoose.Schema({
     time : { type:Date, default:Date.now }
 });
 var TestModel = db.model("test1", TestSchema );
+// var TestEntity = new TestModel({
+//     name : "helloworld",
+//     age  : 28,
+//     email: "helloworld@qq.com"
+// });
+// TestEntity.save(function(error,doc){
+//   if(error){
+//      console.log("error :" + error);
+//   }else{
+//      console.log(doc);
+//   }
+// });
+
+//查询
+// TestModel.find({}, function(error, docs) {
+//     console.log(docs); //若没有向find传递参数，默认的是显示所有文档
+// });
+
+// TestModel.find({ "age": 28 }, function (error, docs) {
+//   if(error){
+//     console.log("error :" + error);
+//   }else{
+//     console.log(docs); //docs: age为28的所有文档
+//   }
+// }); 
+
+//保存方法
+// TestModel.create({
+//     name : "test_create", 
+//     age  : 26, 
+//     email: "tom@qq.com"
+//   },function(error,doc){
+//     console.log(doc);
+// });
+
+//model调用的是create方法，entity调用的是save方法
 var TestEntity = new TestModel({
-    name : "helloworld",
-    age  : 28,
-    email: "helloworld@qq.com"
+    name : "jerry", 
+    age  : 28, 
+    email: "jerry@qq.com"
 });
-TestEntity.save(function(error,doc){
-  if(error){
-     console.log("error :" + error);
-  }else{
-     console.log(doc);
-  }
+TestEntity.save(function(error,doc) {
+   console.log(doc);
 });
