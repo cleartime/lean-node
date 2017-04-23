f39433b1a1261ba4e55f8c1dbe352a052bc63aef
gxx <562606139@qq.com>
2017-04-23 23:12:41 +0800

    4.23

---
+++ egg-example/config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1492960272187_1528';
+
+  return config;
+};
