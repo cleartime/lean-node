0b3b065e33de2f57e1310100f099b4fe04d1d13e
gxx <562606139@qq.com>
2017-11-18 16:32:40 +0800

    1118

--- 171118/test.js
+++ 171118/test.js
@@ -1,11 +1,34 @@
 const fs = require('fs');
 
+const url = '/Users/gxx/Desktop/fxd-wxact/src/views';
+
+
 const readFile = function (fileName) {
   return new Promise(function (resolve, reject) {
     fs.readFile(fileName, function(error, data) {
       if (error) return reject(error);
       resolve(data);
     });
+  });
+};
+
+
+const readDir = function (fileName) {
+  return new Promise(function (resolve, reject) {
+    fs.readdir(fileName, function(error, data) {
+      if (error) return reject(error);
+      resolve(data);
+    });
+  });
+};
+
+
+const lstat = function (fileName) {
+  return new Promise(function (resolve, reject) {
+ 	fs.lstat(fileName, (error, stats)=> {  
+		if (error) return reject(error);
+      	resolve(stats);
+	})
   });
 };
 
@@ -17,8 +40,10 @@ const readFile = function (fileName) {
 // };
 
 const asyncReadFile = async function () {
-  const f1 = await readFile('./test.js');
-  console.log(f2.toString());
+  // const f1 = await readFile(url);
+  const f2 = await lstat(url+'/test.vue');
+  // console.log(f1.toString());
+  console.log(f2)
 };
 
 asyncReadFile();
