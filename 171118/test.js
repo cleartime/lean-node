2d3316ab67714b23ec12706c1a778d0bee31f973
gxx <562606139@qq.com>
2017-11-18 17:06:51 +0800

    1118

--- 171118/test.js
+++ 171118/test.js
@@ -2,15 +2,7 @@ const fs = require('fs');
 
 const url = '/Users/gxx/Desktop/fxd-wxact/src/views';
 
-
-const readFile = function (fileName) {
-  return new Promise(function (resolve, reject) {
-    fs.readFile(fileName, function(error, data) {
-      if (error) return reject(error);
-      resolve(data);
-    });
-  });
-};
+const PeopleGroups = '/Users/gxx/Desktop/fxd-wxact/src/views/PeopleGroups';
 
 
 const readDir = function (fileName) {
@@ -27,7 +19,7 @@ const lstat = function (fileName) {
   return new Promise(function (resolve, reject) {
  	fs.lstat(fileName, (error, stats)=> {  
 		if (error) return reject(error);
-      	resolve(stats);
+		resolve(stats.isDirectory())
 	})
   });
 };
@@ -40,10 +32,30 @@ const lstat = function (fileName) {
 // };
 
 const asyncReadFile = async function () {
-  // const f1 = await readFile(url);
-  const f2 = await lstat(url+'/test.vue');
-  // console.log(f1.toString());
-  console.log(f2)
+  const urlList = await readDir(url);
+  let dirList= []
+  let lastUrlList= []
+  // for(let i = 0,len = urlList.length; i < len;i++){
+  // 	if(await lstat(url+'/'+urlList[i])){
+  // 		f2 = urlList[i]
+  // 	}
+  // }
+  for(let i of urlList){
+ 	if(await lstat(url+'/'+i)){
+  		dirList.push(i)
+  	}
+  }
+
+  for(let i of dirList){
+  	console.log.log(i)
+  	// lastUrlList = await readDir(i);
+  }
+  // urlList.forEach(t=>{
+  // 	f2 = await lstat(t)
+  // 	console.log(f2)
+  // })
+  // console.log(f1);
+  // console.log(dirList)
 };
 
 asyncReadFile();
