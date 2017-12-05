5fc590208957cbb09676606520f03048d9b341ab
gxx <562606139@qq.com>
2017-12-05 23:15:34 +0800

    1205

--- 1205/server.js
+++ 1205/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 
+const expressGraphql = require('express-graphql');
+
 
 const app = express();
 
+
+app.use('/graphql',expressGraphql(()=>{
+	schema,
+	graphql: true,
+	pretty: true
+}))
 
 app.listen(4000,()=>{
     console.log('listen in 4000:')
